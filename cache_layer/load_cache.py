a39117bacd5336b7e18f4181c7e331ed6f2df3b5
Daan <egdaan@gmail.com>
2024-07-22 19:23:34 +0200

    Added check for cache, and return cached body if found

--- cache_layer/load_cache.py
+++ cache_layer/load_cache.py
@@ -1,5 +1,6 @@
 import io
 import json
+import time
 
 
 def load_cache_file(file: str) -> dict:
@@ -19,4 +20,8 @@ def find_cache_file_return_body(cache_file, host, path) -> str:
         return ""
     if path not in cache_file[host]:
         return ""
+
+    if time.time() > cache_file[host][path]["cache_valid_till"]:
+        return ""
+
     return cache_file[host][path]["body"]
